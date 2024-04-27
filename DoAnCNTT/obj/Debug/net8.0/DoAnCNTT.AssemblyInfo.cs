e41aa2beb2c8f431dabd75455bb4c5730945d737
Phong <144468925+NhatTruong200403@users.noreply.github.com>
2024-04-27 15:55:27 +0700

    BanChinhThuc

--- DoAnCNTT/obj/Debug/net8.0/DoAnCNTT.AssemblyInfo.cs
+++ DoAnCNTT/obj/Debug/net8.0/DoAnCNTT.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnCNTT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459c2e28726b260016d7f96008ab84489fcf6902")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fbb01b7cbbd70a56e7c68b0c7ade6075d8ee802")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnCNTT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnCNTT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
